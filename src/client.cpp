f853dc2467463921c76f0f39de0caab761d8cb7d
Adam McCartney <adam.mccartney@tuwien.ac.at>
2024-07-12 16:24:46 +0200

    Ch 6: Non-blocking server

--- src/client.cpp
+++ src/client.cpp
@@ -51,7 +51,7 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
 }
 
 
-static int32_t query(int fd, const char *text) {
+static int32_t send_req(int fd, const char *text) {
   uint32_t len = (uint32_t)strlen(text);
   if (len > k_max_msg) {
     return -1;
@@ -60,9 +60,12 @@ static int32_t query(int fd, const char *text) {
   char wbuf[4 + k_max_msg];
   memcpy(wbuf, &len, 4);
   memcpy(&wbuf[4], text, len);
-  if (int32_t err = write_all(fd, wbuf, 4 + len)) {
-    return err;
-  }
+  
+  return write_all(fd, wbuf, 4 + len);
+}
+
+
+static int32_t read_res(int fd)  {
 
   // 4 bytes header
   char rbuf[4 + k_max_msg + 1];
@@ -77,6 +80,7 @@ static int32_t query(int fd, const char *text) {
     return err;
   }
 
+  uint32_t len = 0;
   memcpy(&len, rbuf, 4); // assume little endian
   if (len > k_max_msg) {
     msg("too long");
@@ -113,19 +117,22 @@ int main(void) {
     die("connect");
   }
 
-  int32_t err = query(fd, "hello1");
-  if (err) {
-    goto L_DONE;
+  // multiple pipelined requests
+  const char *query_list[3] = {"hello1", "hello2", "hello3"};
+  for (size_t i = 0; i < 3; ++i) {
+    int32_t err = send_req(fd, query_list[i]);
+    if (err) {
+        goto L_DONE;
+    }
   }
-  err = query(fd, "hello2");
-  if (err) {
-    goto L_DONE;
-  }
-  err = query(fd, "hello3");
-  if (err) {
-    goto L_DONE;
+  for (size_t i = 0; i < 3; ++i) {
+    int32_t err = read_res(fd);
+    if (err) {
+        goto L_DONE;
+    }
   }
 
+  
 L_DONE:
   close(fd);
   return 0;
