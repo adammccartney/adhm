5afc26ec04358281d849505206e0a47dd23f70fe
Adam McCartney <adam.mccartney@tuwien.ac.at>
2024-07-12 16:59:44 +0200

    Chpt 07: simple protocol implementation

--- src/client.cpp
+++ src/client.cpp
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <vector>
+#include <string>
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
@@ -51,19 +53,29 @@ static int32_t write_all(int fd, const char *buf, size_t n) {
 }
 
 
-static int32_t send_req(int fd, const char *text) {
-  uint32_t len = (uint32_t)strlen(text);
+static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
+  uint32_t len = 4;
+  for (const std::string &s : cmd) {
+    len += 4 + s.size();
+  }
   if (len > k_max_msg) {
     return -1;
   }
-
+  
   char wbuf[4 + k_max_msg];
   memcpy(wbuf, &len, 4);
-  memcpy(&wbuf[4], text, len);
-  
+  uint32_t n = cmd.size();
+  memcpy(&wbuf[4], &n, len);
+  size_t cur = 8;
+  for (const std::string &s : cmd) {
+    uint32_t p = (uint32_t)s.size();
+    memcpy(&wbuf[cur], &p, 4);
+    memcpy(&wbuf[cur + 4], s.data(), s.size());
+    cur += 4 + s.size();
+  }
   return write_all(fd, wbuf, 4 + len);
 }
-
+ 
 
 static int32_t read_res(int fd)  {
 
@@ -94,15 +106,20 @@ static int32_t read_res(int fd)  {
     return err;
   }
 
-  // do something
-  rbuf[4 + len] = '\0';
-  printf("server says %s\n", &rbuf[4]);
+  // Print the result
+  uint32_t rescode = 0;
+  if (len < 4) {
+    msg("bad response");
+    return -1;
+  }
+  memcpy(&rescode, &rbuf[4], 4);
+  printf("server says [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
   return 0;
 }
   
   
 
-int main(void) {
+int main(int argc, char **argv) {
   int fd = socket(AF_INET, SOCK_STREAM, 0);
   if (fd < 0) {
     die("socket()");
@@ -117,22 +134,19 @@ int main(void) {
     die("connect");
   }
 
-  // multiple pipelined requests
-  const char *query_list[3] = {"hello1", "hello2", "hello3"};
-  for (size_t i = 0; i < 3; ++i) {
-    int32_t err = send_req(fd, query_list[i]);
-    if (err) {
-        goto L_DONE;
-    }
+  std::vector<std::string> cmd;
+  for (int i = 1; i < argc; ++i) {
+    cmd.push_back(argv[i]);
   }
-  for (size_t i = 0; i < 3; ++i) {
-    int32_t err = read_res(fd);
-    if (err) {
-        goto L_DONE;
-    }
+  int32_t err = send_req(fd, cmd);
+  if (err) {
+    goto L_DONE;
+  }
+  err = read_res(fd);
+  if (err) {
+    goto L_DONE;
   }
 
-  
 L_DONE:
   close(fd);
   return 0;
--- src/server.cpp
+++ src/server.cpp
@@ -1,3 +1,4 @@
+#include <map>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,16 +12,60 @@
 #include <fcntl.h>
 #include <poll.h>
 #include <vector>
+#include <string>
 
 const size_t k_max_msg = 4096;
 
 enum {
-    STATE_REQ = 0,
-    STATE_RES = 1,
-    STATE_END = 2, // mark the connection for deletion
+  STATE_REQ = 0,
+  STATE_RES = 1,
+  STATE_END = 2, // mark the connection for deletion
 };
 
-typedef struct Conn {
+enum {
+  RES_OK = 0,
+  RES_ERR = 1,
+  RES_NX = 2,
+};
+
+// The data structure for the key space. This is just a placeholder
+// until we implement a hashable table in the next chapter.
+static std::map<std::string, std::string> g_map;
+
+/*
+  Protocol Commands
+ */
+
+static uint32_t do_get(const std::vector<std::string> &cmd, uint8_t *res,
+                       uint32_t *reslen) {
+  if (!g_map.count(cmd[1])) {
+    return RES_NX;
+  }
+  std::string &val = g_map[cmd[1]];
+  assert(val.size() <= k_max_msg);
+  memcpy(res, val.data(), val.size());
+  *reslen = (uint32_t)val.size();
+  return RES_OK;
+}
+
+static uint32_t do_set(const std::vector<std::string> &cmd, uint8_t *res,
+                       uint32_t *reslen) {
+  (void)res;
+  (void)reslen;
+  g_map[cmd[1]] = cmd[2];
+  return RES_OK;
+}
+
+static uint32_t do_del(const std::vector<std::string> &cmd, uint8_t *res,
+                       uint32_t *reslen) {
+  (void)res;
+  (void)reslen;
+  g_map.erase(cmd[1]);
+  return RES_OK;
+}
+
+
+struct Conn {
     int fd = -1;
     uint32_t state = 0;  // either STATE_REQ or STATE_RES
     // buffer for reading
@@ -30,7 +75,7 @@ typedef struct Conn {
     size_t wbuf_size = 0;
     size_t wbuf_sent = 0;
     uint8_t wbuf[4 + k_max_msg];
-} Conn;
+};
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
@@ -213,6 +258,65 @@ static void state_res(Conn *conn) {
   }
 }
 
+static int32_t parse_req(const uint8_t *data, size_t len,
+                         std::vector<std::string> &out) {
+  if (len < 4) {
+    return -1;
+  }
+  uint32_t n = 0;
+  memcpy(&n, &data[0], 4);
+  if (n > k_max_msg) {
+    return -1;
+  }
+
+  size_t pos = 4;
+  while (n--) {
+    if (pos + 4 > len) {
+      return -1;
+    }
+    uint32_t sz = 0;
+    memcpy(&sz, &data[pos], 4);
+    if (pos + 4 + sz > len) {
+      return -1;
+    }
+    out.push_back(std::string((char *)&data[pos + 4], sz));
+    pos += 4 + sz;
+  }
+
+  if (pos != len) {
+    return -1; // trailing garbage
+  }
+  return 0;
+}
+
+static bool cmd_is(const std::string &word, const char *cmd) {
+  return 0 == strcasecmp(word.c_str(), cmd);
+}
+
+static int32_t do_request(const uint8_t *req, uint32_t reqlen,
+                          uint32_t *rescode, uint8_t *res, uint32_t *reslen) {
+  std::vector<std::string> cmd;
+  if (0 != parse_req(req, reqlen, cmd)) {
+    msg("bad req");
+    return -1;
+  }
+  if (cmd.size() == 2 && cmd_is(cmd[0], "get")) {
+    *rescode = do_get(cmd, res, reslen);
+  } else if (cmd.size() == 3 && cmd_is(cmd[0], "set")) {
+    *rescode = do_set(cmd, res, reslen);
+  } else if (cmd.size() == 2 && cmd_is(cmd[0], "del")) {
+    *rescode = do_del(cmd, res, reslen);
+  } else {
+    // cmd is not recognized
+    *rescode = RES_ERR;
+    const char *msg = "Unknown cmd";
+    strcpy((char *)res, msg);
+    *reslen = strlen(msg);
+    return 0;
+  }
+  return 0;
+}
+
 static bool try_one_request(Conn *conn) {
   // try to parse a request from the buffer
   if (conn->rbuf_size < 4) {
@@ -231,13 +335,19 @@ static bool try_one_request(Conn *conn) {
     return false;
   }
 
-  // got one request, do something with it
-  printf("client says: %.*s\n", len, &conn->rbuf[4]);
-
-  // generating echo response
-  memcpy(&conn->wbuf[0], &len, 4);
-  memcpy(&conn->wbuf[4], &conn->rbuf[4], len);
-  conn->wbuf_size = 4 + len;
+  // got one request, generate the response.
+  uint32_t rescode = 0;
+  uint32_t wlen = 0;
+  int32_t err = do_request(&conn->rbuf[4], len, &rescode, &conn->wbuf[4 + 4],
+                         &wlen);
+  if (err) {
+    conn->state = STATE_END;
+    return false;
+  }
+  wlen += 4;
+  memcpy(&conn->wbuf[0], &wlen, 4);
+  memcpy(&conn->wbuf[4], &rescode, 4);
+  conn->wbuf_size = 4 + wlen;
 
   // remove the request from buffer
   // note: frequent memove is inefficient
